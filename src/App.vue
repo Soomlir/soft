fb0855515afc3f9e6bfec9b6fcf3b981fe00e433
soomlir <soomlir@yandex.ru>
2025-03-07 20:52:35 +0300

    stores, app.vue edit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,146 @@
 <script setup lang="ts">
+import { useUsersStore } from "./stores/UsersStore.ts";
+
+interface User {
+  id: number;
+  mark: Array<{ text: string }>;
+  type: string;
+  login: string;
+  password: string;
+  showPassword: boolean;
+  isError: boolean;
+}
+
+const usersStore = useUsersStore();
+
+const toggleShowPassword = (index: number) => {
+  usersStore.usersList[index].showPassword =
+    !usersStore.usersList[index].showPassword;
+};
+
+const addNewUser = () => {
+  usersStore.addUser({
+    id: Date.now(),
+    mark: [{ text: "" }],
+    type: "local",
+    login: "",
+    password: "",
+    showPassword: false,
+    isError: false,
+  });
+};
+
+const deleteUser = (id: number) => {
+  usersStore.removeUser(id);
+};
+
+const isLoginError = (login: string) => {
+  return !login || login.length < 4;
+};
+
+const isPasswordError = (password: string) => {
+  return !password || password.length < 4;
+};
+
+const updateField = (index: number, field: any, value: string) => {
+  usersStore.usersList[index][field] = value;
+  console.log(usersStore.usersList[index][field]);
+  localStorage.setItem("usersList7", JSON.stringify(usersStore.usersList));
+};
+
+const updateType = (index: number, value: string) => {
+  usersStore.usersList[index]["type"] = value;
+  if (usersStore.usersList[index]["type"] == "ldap") {
+    usersStore.usersList[index]["password"] = null;
+  }
+  localStorage.setItem("usersList7", JSON.stringify(usersStore.usersList));
+};
+
+const getFormattedMark = (user: User) => {
+  return user.mark.map((item) => item.text).join("; ");
+};
+
+const updateMark = (user: User, value: string) => {
+  const newMark = value.split(";").map((text) => ({ text: text.trim() }));
+
+  usersStore.updateUserMark(user.id, newMark);
+  localStorage.setItem("usersList7", JSON.stringify(usersStore.usersList));
+};
 </script>
 
 <template>
-4444
+  <section>
+    <h1>Управление учетными записями</h1>
+    <p>Учетные записи <button @click="addNewUser">+</button></p>
+    <p>
+      Для указания нескольких меток для одной пары логин/пароль используйте
+      разделитель ;
+    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Метки</th>
+          <th>Тип записи</th>
+          <th>Логин</th>
+          <th>Пароль</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in usersStore.usersList" :key="user.id">
+          <td>
+            <input
+              :value="getFormattedMark(user)"
+              @input="(e) => updateMark(user, e.target?.value || '')"
+              placeholder="Метка"
+            />
+          </td>
+          <td>
+            <select v-model="user.type" @change="updateType(index, user.type)">
+              <option value="local">Локальная</option>
+              <option value="ldap">LDAP</option>
+            </select>
+          </td>
+          <td>
+            <input
+              type="text"
+              v-model="user.login"
+              placeholder="Login"
+              @blur="updateField(index, 'login', user.login)"
+              maxlength="100"
+              :class="{ 'error-field': isLoginError(user.login) }"
+            />
+          </td>
+          <td v-if="user.type == 'local'">
+            <input
+              :type="user.showPassword ? 'text' : 'password'"
+              v-model="user.password"
+              placeholder="Password"
+              @blur="updateField(index, 'password', user.password)"
+              maxlength="100"
+              :class="{ 'error-field': isPasswordError(user.password) }"
+            />
+            <button type="button" @click="toggleShowPassword(index)">sh</button>
+          </td>
+          <td>
+            <button @click="deleteUser(user.id)">del</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <style scoped>
+th {
+  text-align: left;
+}
+
+input {
+  border: 2px solid black;
+}
+
+.error-field {
+  border-color: red;
+}
 </style>
---
+++ src/stores/UsersStore.ts
@@ -0,0 +1,75 @@
+import { defineStore } from "pinia";
+import { ref, watch } from "vue";
+
+interface User {
+  id: number;
+  mark: any;
+  type: string;
+  login: string;
+  password: string;
+  showPassword: boolean;
+  isError: boolean
+}
+
+export const useUsersStore = defineStore("usersStore", () => {
+  const usersList = ref<User[]>([]);
+
+  const addUser = (newUser: any) => {
+    usersList.value.push(newUser);
+  };
+
+  const removeUser = (index: number) => {
+    usersList.value = usersList.value.filter((item) => item.id != index);
+  };
+
+  const loadUsersFromLocalStorage = () => {
+    const storedUsers = localStorage.getItem("usersList7");
+    console.log("Загрузка из localStorage:", storedUsers);
+    if (storedUsers) {
+      usersList.value = JSON.parse(storedUsers);
+    } else {
+      usersList.value = [
+        {
+          id: 1,
+          mark: [{text: 'text1'}],
+          type: "local",
+          login: "admin",
+          password: "123123",
+          showPassword: false,
+          isError: false,
+        },
+        {
+          id: 2,
+          mark: [{text: 'text'}, {text: 'text'}],
+          type: "ldap",
+          login: "user",
+          password: "777",
+          showPassword: false,
+          isError: false,
+        },
+      ];
+      saveUsersToLocalStorage();
+    }
+  };
+
+  const saveUsersToLocalStorage = () => {
+    try {
+      localStorage.setItem("usersList7", JSON.stringify(usersList.value));
+      console.log("Данные успешно сохранены в localStorage");
+    } catch (error) {
+      console.error("Ошибка при сохранении в localStorage:", error);
+    }
+  };
+
+  const updateUserMark = (id: number, mark: Array<{ text: string }>) => {
+    const user = usersList.value.find(user => user.id === id);
+    if (user) {
+      user.mark = mark;
+    }
+  };
+
+  loadUsersFromLocalStorage();
+  watch(usersList, saveUsersToLocalStorage);
+
+  return { usersList, addUser, removeUser, updateUserMark };
+});
